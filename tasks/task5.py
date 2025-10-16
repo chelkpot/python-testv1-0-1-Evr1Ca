4234a8805d75e9047161e62359633392c43708ee
Evr1Ca <kalikusarka@gmail.com>
2025-10-16 22:52:35 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,7 +4,9 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
-
+    numbers = map(int, input("Введите числа: ").split())
+    squares = map(lambda x: x**2, numbers)
+    print("Результат:", *squares)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
